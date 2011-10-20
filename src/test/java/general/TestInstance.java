18df148303a747ac9577951c48bf5cd238743d07
Dominik Hansen <dominik_hansen@web.de>
2011-10-20 15:01:51 +0000

    Fixed file separator bug
    git-svn-id: https://cobra.cs.uni-duesseldorf.de/prob/trunk/tla@9290 7aec93f6-bc54-0410-ac70-7d7c9efa889a

--- src/test/java/general/TestInstance.java
+++ src/test/java/general/TestInstance.java
@@ -4,42 +4,40 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import translation.Main;
+import util.FileUtil;
 import util.ToolIO;
 import static org.junit.Assert.assertEquals;
 import static util.TestUtil.getTreeAsString;
 import static util.TestUtil.fileToString;
 
 public class TestInstance {
+	private String path = "src" + FileUtil.separator + "test"
+			+ FileUtil.separator + "resources" + FileUtil.separator
+			+ "instance" + FileUtil.separator;
 
 	@Test
 	public void TestNamedInstance() throws Exception {
 		ToolIO.setMode(ToolIO.TOOL);
-		String dir = "src\\test\\resources\\instance\\test1\\";
-		StringBuilder sb = Main.start(dir +"Main", null, false);
+		StringBuilder sb = Main.start(path+ "test1"+ FileUtil.separator + "Main", null, false);
 		String result = getTreeAsString(sb.toString());
-		String expected = getTreeAsString(fileToString(dir +"Main.mch"));
+		String expected = getTreeAsString(fileToString(path+ "test1"+ FileUtil.separator + "Main.mch"));
 		assertEquals(expected, result);
 	}
 
 	@Test
 	public void TestUnnamedInstance() throws Exception {
-		String dir = "src\\test\\resources\\instance\\test2\\";
-		StringBuilder sb = Main.start(dir +"Main", null, false);
+		StringBuilder sb = Main.start(path+ "test2"+ FileUtil.separator + "Main", null, false);
 		String result = getTreeAsString(sb.toString());
-		String expected = getTreeAsString(fileToString(dir +"Main.mch"));
+		String expected = getTreeAsString(fileToString(path+ "test2"+ FileUtil.separator + "Main.mch"));
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
 	public void TestNoSubstitution() throws Exception {
-		String dir = "src\\test\\resources\\instance\\test3\\";
-		StringBuilder sb = Main.start(dir +"Main", null, false);
+		StringBuilder sb = Main.start(path+ "test2"+ FileUtil.separator + "Main", null, false);
 		String result = getTreeAsString(sb.toString());
-		String expected = getTreeAsString(fileToString(dir +"Main.mch"));
+		String expected = getTreeAsString(fileToString(path+ "test2"+ FileUtil.separator + "Main.mch"));
 		assertEquals(expected, result);
 	}
-	
-	
-	
 
 }
