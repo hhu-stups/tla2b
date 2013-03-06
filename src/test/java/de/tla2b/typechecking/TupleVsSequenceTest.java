605d0e81a2db6e08803a5092e42b6722fa71a697
Dominik Hansen <Dominik.Hansen@uni-duesseldorf.de>
2013-03-06 11:20:21 +0100

    TLA2B now distinguishes between tuples and sequences.

---
+++ src/test/java/de/tla2b/typechecking/TupleVsSequenceTest.java
@@ -0,0 +1,50 @@
+package de.tla2b.typechecking;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import de.tla2b.exceptions.FrontEndException;
+import de.tla2b.exceptions.TLA2BException;
+import de.tla2b.util.TestTypeChecker;
+import de.tla2b.util.Util;
+
+
+public class TupleVsSequenceTest {
+
+	@Test  
+	public void testTupleVsSequence() throws FrontEndException, TLA2BException {
+		final String module = "-------------- MODULE Testing ----------------\n"
+				+ "EXTENDS Sequences \n"
+				+ "CONSTANTS k \n"
+				+ "ASSUME k = Append(<<>>, TRUE) \n"
+				+ "=================================";
+		TestTypeChecker t = Util.typeCheckString(module);
+		assertEquals("POW(INTEGER*BOOL)", t.getConstantType("k"));
+	}
+	
+	@Test  
+	public void testTupleVsSequence2() throws FrontEndException, TLA2BException {
+		final String module = "-------------- MODULE Testing ----------------\n"
+				+ "EXTENDS Sequences \n"
+				+ "CONSTANTS k, k2 \n"
+				+ "ASSUME k = Append(<<1,2,3>>, k2) \n"
+				+ "=================================";
+		TestTypeChecker t = Util.typeCheckString(module);
+		assertEquals("POW(INTEGER*INTEGER)", t.getConstantType("k"));
+		assertEquals("INTEGER", t.getConstantType("k2"));
+	}
+	
+	@Test  
+	public void testTupleVsSequence3() throws FrontEndException, TLA2BException {
+		final String module = "-------------- MODULE Testing ----------------\n"
+				+ "CONSTANTS a, b, c\n"
+				+ "ASSUME <<1,2,3,4>> = <<a,b,c>> \n"
+				+ "=================================";
+		TestTypeChecker t = Util.typeCheckString(module);
+		assertEquals("INTEGER", t.getConstantType("a"));
+		assertEquals("INTEGER", t.getConstantType("b"));
+		assertEquals("INTEGER", t.getConstantType("c"));
+	}
+	
+}
